78d42fae8027afa27d9786393475a20b13711db7
Abdul Haseeb Memon <142418450+Haseeb1717@users.noreply.github.com>
2024-07-15 18:34:49 +0400

    Create productly.js
    js file

---
+++ productly.js
@@ -0,0 +1,7 @@
+        document.querySelector('.menu-icon').addEventListener('click', function() {
+            document.querySelector('.navlist').classList.toggle('active');
+        });
+     // Initialize AOS
+  AOS.init({
+    duration: 800 // Set your desired speed here (in milliseconds)
+  });
